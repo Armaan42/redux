2f147ac978d668e842586eea559a8b4bef5138f5
Armaan <armaansinghbhau8@gmail.com>
2026-06-30 21:33:19 +0530

    write a function for fetchphotos and fetchvideos with api's

--- project/src/api/mediaApi.js
+++ project/src/api/mediaApi.js
@@ -4,14 +4,31 @@ const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY
 
-export function fetchPhotos(query) {
-    const res = axios.get('https://api.unsplash.com/search/photos', {
+export async function fetchPhotos(query, page=1, per_page=20) {
+    const res = await axios.get('https://api.unsplash.com/search/photos', {
         params:{
-    
+            query,
+            page,
+            per_page
         },
         headers:{
-
+            Authorization:`Client-ID ${UNSPLASH_KEY}`
         }
     })
-    console.log(res);
+    
+    return res.data
+}
+
+export async function fetchVideos(query, per_page=20) {
+    const res = await axios.get('https://api.pexels.com/v1/videos/search', {
+        params:{
+            query,
+            per_page
+        },
+        headers:{
+            Authorization:`${PEXELS_KEY}`
+        }
+    })
+    
+    return res.data 
 }

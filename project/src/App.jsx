5da7a3738267b2e54ec7f0ce20bdc82c3ea1019a
Armaan <armaansinghbhau8@gmail.com>
2026-06-30 21:32:23 +0530

    Added the Get photos and Get Videos button

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { fetchPhotos, fetchVideos } from './api/mediaApi'
 
 const App = () => {
+
+  // function getPhotos() {
+  //   fetchPhotos()
+  // }
+
   return (
-    <div>
-      App
+    <div className='h-screen w-full bg-[#1e1e1e] text-white'>
+
+      {/* Get photos */}
+      <button className='bg-red-300 p-3 m-3 rounded-2xl text-black' 
+
+      onClick={ async () => {
+        const data = await fetchPhotos('cat')
+        console.log(data.results);
+        
+      }}>Get photos</button>
+
+      {/* Get Videos */}
+      <button className='bg-red-300 p-3 m-3 rounded-2xl text-black' 
+      onClick={ async () => {
+        const data = await fetchVideos('cat')
+        console.log(data.videos);
+      }}>
+      Get Videos</button>
+
     </div>
   )
 }

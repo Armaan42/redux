ad9f1171815aebd42d99304d73daee3072d6d75b
Armaan <armaansinghbhau8@gmail.com>
2026-06-30 11:15:22 +0530

    Did 2-way binding to update the count based on the user choice and added the input field for that where using useState hook we did that

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decreaseByFive, decrement, increaseByFive, increment, incrementByAmount, } from './redux/features/counterSlice'
 
 const App = () => {
-
   // yeah button ke click pe joh action karne the vo keraga
   const dispatch = useDispatch() 
 
   // yeah counter ke value ko showcase karega current bali
   const count = useSelector((state) => state.counter.value) 
+
+  const [num, setNum] = useState(5)
 
   return (
     <div>
@@ -31,11 +32,16 @@ const App = () => {
         dispatch(decreaseByFive())
       }}>decrease By Five</button>
 
+      <input type="number" value={num} onChange={(e) => {
+        setNum(e.target.value);
+
+      }} />
       <button onClick={() => {
-        dispatch(incrementByAmount(10))
-      }} >
-        increment by Amount
+        dispatch(incrementByAmount(Number(num)))
+      }}>increment by Amount
       </button>
+
+
 
     </div>
   )

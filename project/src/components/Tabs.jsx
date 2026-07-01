fe826413329d547d050ef018a24d32400635376a
Armaan <armaansinghbhau8@gmail.com>
2026-07-01 19:59:52 +0530

    Added the button to select the activeTab

--- project/src/components/Tabs.jsx
+++ project/src/components/Tabs.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setActiveTabs } from '../redux/features/searchSlice'
 
 const Tabs = () => {
     const tabs = ['photos', 'videos', 'GIF']
+
+    const dispatch = useDispatch()
+
+    const activeTab = useSelector((state) => state.search.activeTab)
 
   return (
     <div className='flex gap-5 p-10'>
 
       {tabs.map(function(elem, idx){
-        return <button className='bg-gray-500 cursor-pointer active:scale-95 px-5 py-2 rounded-2xl uppercase' key={idx}>{elem}</button>
-      })}
+        return ( 
+        <button 
+        className={`${(activeTab == elem ? 'bg-blue-900' : 'bg-gray-500')}  transition cursor-pointer active:scale-95 px-5 py-2 rounded-2xl 
+            uppercase`}
+            key={idx}
+         onClick={() => {
+            dispatch(setActiveTabs(elem))
+        }}>{elem}
+        </button>
 
+    )
+      })}
     </div>
   )
 }

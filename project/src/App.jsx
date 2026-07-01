4467d5f9230f6e59f3abd9367308e1783079b96f
Armaan <armaansinghbhau8@gmail.com>
2026-07-01 18:40:17 +0530

    Added the searchbar and tabs components

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,40 +1,15 @@
 import React from 'react'
-import { fetchPhotos, fetchVideos, fetchGif } from './api/mediaApi'
+import {  } from './api/mediaApi'
+import SearchBar from './components/SearchBar'
+import Tabs from './components/Tabs'
 
 const App = () => {
-
-  // function getPhotos() {
-  //   fetchPhotos()
-  // }
 
   return (
     <div className='h-screen w-full bg-[#1e1e1e] text-white'>
 
-      {/* Get photos */}
-      <button className='bg-red-300 p-3 m-3 rounded-2xl text-black' 
-
-      onClick={ async () => {
-        const data = await fetchPhotos('cat')
-        console.log(data.results);
-        
-      }}>Get photos</button>
-
-      {/* Get Videos */}
-      <button className='bg-red-300 p-3 m-3 rounded-2xl text-black' 
-      onClick={ async () => {
-        const data = await fetchVideos('cat')
-        console.log(data.videos);
-      }}>
-      Get Videos</button>
-
-
-      {/* Get Gifs */}
-      <button className='bg-red-300 p-3 m-3 rounded-2xl text-black' 
-      onClick={ async () => {
-        const data = await fetchGif('cat')
-        console.log(data);
-      }}>
-      Get Gifs</button>
+      <SearchBar />
+      <Tabs />
 
     </div>
   )
